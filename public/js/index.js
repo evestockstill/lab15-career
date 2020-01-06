8642db086c084203c2fdee67aef494a2aada8bdc
Eve Stockstill <evestockstillpdx@gmail.com>
2020-01-05 23:13:53 -0800

    started a front end

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 
 
 const root = document.getElementById('root');
@@ -5,12 +6,33 @@ const root = document.getElementById('root');
 const displayForm = () => {
   const form = document.createElement('form');
   form.innerHTML = `
-<fieldset>
-  <legend>Signup</legend>
-  <input name="email" type="text" placeholder="email">
-  <input name="password" type="password" placeholder="password">
-</fieldset>
-<button>Signup</button>
+  <section>
+      <div>
+        <div class="sign-up">
+          <div class="signup__form">
+            <form action="#" class="form">
+              <div>
+                <h2 class="heading">
+                Sign up for Films and reviews
+                </h2>
+              </div>
+              <div class="form__group">
+                <input type="email" class="form__input" placeholder="Email address" id="email" required>
+                <label for="email" class="form__label">Email Address</label>
+              </div>
+              <div class="form__group">
+                <input type="password" class="form__input" placeholder="Password" id="password" required>
+                <label for="password" class="form__label">Password</label>
+              </div>
+              </div>
+              <div class="form__group">
+                <button class="sign">Next step &rarr;</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
 `;
 
   form
@@ -63,8 +85,8 @@ fetch('/api/v1/auth/verify', {
 })
   .then(res => res.json())
   .then(user => {
-    if (user._id) {
-      window.location.href = '/index.html';
+    if(user._id) {
+      window.location.href = 'Film/index.html';
     } else {
       displayLogin();
     }
